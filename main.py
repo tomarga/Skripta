209f1d83fad15b537cb1bc54bca58af267e50edb
margarita <margarita@buckhill.co.uk>
2021-10-17 23:20:38 +0200

    Add initial window design

---
+++ main.py
@@ -0,0 +1,23 @@
+from PyQt6 import QtWidgets, QtGui
+
+from MainWindow import Ui_MainWidget
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    MainWidget = QtWidgets.QWidget()
+    ui = Ui_MainWidget()
+    ui.setupUi(MainWidget)
+
+    window = QtWidgets.QMainWindow()
+    window.setWindowTitle("Skripta")
+    icon = QtGui.QIcon()
+    icon.addPixmap(QtGui.QPixmap("resources/favicon.png"), QtGui.QIcon.Mode.Normal, QtGui.QIcon.State.Off)
+    window.setWindowIcon(icon)
+    window.setFixedSize(800, 800)
+    window.setCentralWidget(MainWidget)
+
+    window.show()
+    sys.exit(app.exec())
