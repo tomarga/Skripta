2d5cbba865ab8354e9593007a29e457d01b69a43
margarita <tomarga@student.math.hr>
2022-01-03 19:57:36 +0100

    Util: Update docstring, add textual extension getter method

--- src/Util.py
+++ src/Util.py
@@ -23,7 +23,7 @@ class Util:
     def listExtensionsAsString(extensions):
         """
         For given list of file extensions, returns the string "(*ext1 *ext2 ... *extn)".
-        The result format is customized to match the 'open file dialog' supported extensions' parameter.
+        The result format is customized to match the QFile's 'open/save file dialog' supported extensions' parameter.
         :param extensions: List of string containing file extensions.
         :return: A string like "(*ext1 *ext2 ... *extn)".
         """
@@ -34,3 +34,31 @@ class Util:
         result = result[:-1]
         result += ')'
         return result
+
+    @staticmethod
+    def listExtensionsAsString(extensions):
+        """
+        For given list of file extensions, returns the string "(*ext1 *ext2 ... *extn)".
+        The result format is customized to match the QFile's 'open/save file dialog' supported extensions' parameter.
+        :param extensions: List of string containing file extensions.
+        :return: A string like "(*ext1 *ext2 ... *extn)".
+        """
+
+        result = "("
+        for extension in extensions:
+            result += '*' + extension + ' '
+        result = result[:-1]
+        result += ')'
+        return result
+
+    @staticmethod
+    def getTextualExtensions():
+        """
+        Returns the string containing a selection of textual file extensions.".
+        The result format is customized to match the QFile's 'open/save file dialog' supported extensions' parameter.
+        :return: A string like "(TypeName1 *ext1;;TypeName2 *ext2;; ... ;;TypeNamen *extn)".
+        """
+
+        return "All formats (*odt *docx *uot *xml *tex *txt *html);;" \
+               "ODF Text Document (*odt);;Word 2007-365 (*docx);;Unified Office Format text (*uot);;Word 2003 XML " \
+               "(*xml);;HTML (*html);;TeX (*tex);;Rich text (*rtf);; Text(*txt)"
