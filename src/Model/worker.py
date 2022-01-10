0864b9d2bc940c3109d709b948b3d215c29d9a49
margarita <tomarga@student.math.hr>
2022-01-11 00:19:08 +0100

    worker: Setup command line arguments parser

--- src/Model/worker.py
+++ src/Model/worker.py
@@ -1,36 +1,40 @@
-import sys
+import argparse
 
-import speech_recognition as sr
+from EnergyThresholdOption import EnergyThresholdOption
+from API import API
+from Recognizer import Recognizer
 
 
-def run(filePath: str):
+def setupParser():
     """
-    Initializes a recognizer instance and runs the transcription of the file whose path is given as an argument.
-    Outputs the result data to standard output channel if successful.
-    Outputs the error message to standard error channel otherwise.
-    :param filePath: Path to an audio file.
-    :return:
+    Setups parser for command line arguments.
+    :return: Parser instance with defined arguments.
     """
 
-    recognizer = sr.Recognizer()
-    audioFile = sr.AudioFile(filePath)
+    newParser = argparse.ArgumentParser()
 
-    try:
-        with audioFile as source:
-            audio = recognizer.record(source)
-    except ValueError as e:
-        sys.stderr.write("ValueError - Audio as Source: " + e.__str__())
-        return
+    # file
+    newParser.add_argument("file", type=str, help="the audio file path")
 
-    try:
-        resultData = recognizer.recognize_google(audio)
-        sys.stdout.write(resultData)
+    # basic options
+    newParser.add_argument("-e", "--energy", type=lambda energy: EnergyThresholdOption[energy.upper()],
+                           help="one of the tree energy threshold options", choices=list(EnergyThresholdOption),
+                           default=EnergyThresholdOption.DYNAMIC)
 
-    except sr.RequestError as e:
-        sys.stderr.write("RequestError - Transcription" + e.__str__())
+    newParser.add_argument("-sv", "--start_value", type=int, help="starter value for non-dynamic threshold options")
+    newParser.add_argument("-o", "--offset", type=int, help="offset of audio file in secs")
+    newParser.add_argument("-d", "--duration", type=int, help="duration of audio file in secs")
 
-    except sr.UnknownValueError as e:
-        sys.stderr.write("UnknownValueError - Transcription" + e.__str__())
+    # api-specific options
+    newParser.add_argument("-a", "--api", type=lambda api: API[api.upper()], help="one of the 4 supported APIs",
+                           choices=list(API), default=API.GOOGLE)
+
+    newParser.add_argument("-l", "--language", type=str, help="language in the audio file", default="en-US")
+    newParser.add_argument("-p", "--phrases", nargs="*", type=str, help="preferred phrases")
+    newParser.add_argument("-pv", "--phrases_values", nargs="*", type=float, help="sensitivity values of preferred phrases")
+    newParser.add_argument("-g", "--grammar", type=str, help=".gram file path")
+
+    return newParser
 
 
 if __name__ == '__main__':
@@ -39,7 +43,17 @@ if __name__ == '__main__':
     The results are printed to standard (error) output channels.
     """
 
-    audioFilePath = sys.argv[1]
+    parser = setupParser()
+    args = parser.parse_args()
 
-    if audioFilePath:
-        run(audioFilePath)
+    phrases = args.phrases
+    if args.phrases_values:
+        for index, phrase in enumerate(args.phrases):
+            phrases[index] = (phrase, args.phrases_values[index])
+
+    file = args.file
+    basicOptions = Recognizer.BasicOptions(args.energy, args.start_value, args.offset, args.duration)
+    apiOptions = Recognizer.APIOptions(args.api, args.language, phrases, args.grammar)
+
+    worker = Recognizer(file, basicOptions, apiOptions)
+    worker.run()
