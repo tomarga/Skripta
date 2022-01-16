7d0d56a95acee36740524265b5f23cd26e78f25d
margarita <tomarga@student.math.hr>
2022-01-16 18:49:42 +0100

    Add all microphone input options to worker process

--- src/Model/worker.py
+++ src/Model/worker.py
@@ -13,17 +13,25 @@ def setupParser():
 
     newParser = argparse.ArgumentParser()
 
-    # file
-    newParser.add_argument("file", type=str, help="the audio file path")
+    # input type
+    newParser.add_argument("input", type=str, help="the input type, should be 'file' or 'mic")
 
-    # basic options
+    # file options
+    newParser.add_argument("-f", "--file", type=str, help="the audio file path")
+    newParser.add_argument("-o", "--offset", type=int, help="offset of audio file in secs")
+    newParser.add_argument("-d", "--duration", type=int, help="duration of audio file in secs")
+
+    # mic options
+    newParser.add_argument("-m", "--mic", type=int, help="ordinal number of the microphone input")
+    newParser.add_argument("-st", "--speech_timeout", type=int, help="number of seconds of speech to listen")
+    newParser.add_argument("-hw", "--hotwords", type=str, help="path to the hotwords file")
+
+    # common options
     newParser.add_argument("-e", "--energy", type=lambda energy: EnergyThresholdOption[energy.upper()],
                            help="one of the tree energy threshold options", choices=list(EnergyThresholdOption),
                            default=EnergyThresholdOption.DYNAMIC)
 
     newParser.add_argument("-sv", "--start_value", type=int, help="starter value for non-dynamic threshold options")
-    newParser.add_argument("-o", "--offset", type=int, help="offset of audio file in secs")
-    newParser.add_argument("-d", "--duration", type=int, help="duration of audio file in secs")
 
     # api-specific options
     newParser.add_argument("-a", "--api", type=lambda api: API[api.upper()], help="one of the 4 supported APIs",
@@ -37,6 +45,34 @@ def setupParser():
     return newParser
 
 
+def getTranscriptionOptions():
+    """
+    Creates the instance of MicOptions, FileOptions and CommonOptions based on the command line inputs.
+    :return: Tuple (mic_options, file_options, common_options)
+    """
+
+    micOptions = fileOptions = None
+
+    # file options
+    if args.input == 'file':
+        fileOptions = Recognizer.FileOptions(args.file, args.offset, args.duration)
+
+    # mic options
+    elif args.input == 'mic':
+        micOptions = Recognizer.MicOptions(args.mic, args.speech_timeout, args.hotwords)
+
+    # common options
+    phrases = args.phrases
+    if args.phrases_values:
+        for index, phrase in enumerate(args.phrases):
+            # values are 'inverted' because of the sphinx package implementation
+            phrases[index] = (phrase, 1 - args.phrases_values[index])
+
+    commonOptions = Recognizer.CommonOptions(args.energy, args.start_value, args.api, args.language, phrases, args.grammar)
+
+    return (micOptions, fileOptions, commonOptions)
+
+
 if __name__ == '__main__':
     """
     Runs the transcription of the file whose path is given as command line input.
@@ -46,16 +82,7 @@ if __name__ == '__main__':
     parser = setupParser()
     args = parser.parse_args()
 
-    phrases = args.phrases
-    if args.phrases_values:
-        for index, phrase in enumerate(args.phrases):
+    options = getTranscriptionOptions()
 
-            # values are 'inverted' because of the sphinx package implementation
-            phrases[index] = (phrase, 1 - args.phrases_values[index])
-
-    file = args.file
-    basicOptions = Recognizer.BasicOptions(args.energy, args.start_value, args.offset, args.duration)
-    apiOptions = Recognizer.APIOptions(args.api, args.language, phrases, args.grammar)
-
-    worker = Recognizer(file, basicOptions, apiOptions)
+    worker = Recognizer(*options)
     worker.run()
